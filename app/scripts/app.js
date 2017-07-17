8c08a7827428ba7ca14f49c69c96d8e5d3f38f76
Christopher Sporn <christophersporn@Christophers-iMac.local>
2017-07-17 18:29:34 -0400

    assignment-3-updated

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,26 +2,27 @@
   function config($stateProvider, $locationProvider){
     $locationProvider
         .html5Mode({
-            enabled:true,
+            enabled: true,
             requireBase: false
         });
     $stateProvider
         .state('landing', {
           url: '/',
-          templateUrl: 'templates/landing.html'
+          templateUrl: '/templates/landing.html'
         })
+
         .state('album', {
           url: '/album',
           templateUrl: '/templates/album.html'
         })
-        .state('collection',{
-          url: '/collection'
+        
+        .state('collection', {
+          url: '/collection',
           templateUrl: '/templates/collection.html'
         });
 
-
   }
     angular
-        .module('blocJams', ['ui.router']);
-        .config(config)
+        .module('blocJams', ['ui.router'])
+        .config(config);
 })();
