807fa045c5ecbe922b5421d59ee100f12e810bef
Christopher Sporn <christophersporn@Christophers-iMac.local>
2017-07-27 21:43:21 -0400

    realassignment-5-completed

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,14 +1,7 @@
 (function(){
     function AlbumCtrl(){
 
-      this.albumData = [];
-      for(var i=0; i < 5; i++){
-        this.albumData.push(angular.copy(albumPicasso));
-
-      }
-      this.Title = "The Colors";
-      this.albumArtist = "Pablo Picasso";
-
+    this.albumData = angular.copy(albumPicasso)
 
     }
 
